7dc99e5b04cb96c72b8ac26ec9e40e01506626ad
EvgenStr <juve2910@gmail.com>
2018-04-10 13:36:47 +0300

    button

---
+++ buttons/js.js
@@ -0,0 +1,61 @@
+var add = document.querySelector(".add");
+var ul = document.querySelector("ul");
+var li = document.createElement("li");
+
+add.onclick = function() {
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  var li = document.createElement("li");
+  li.innerHTML += `${h}:${m}:${s}`;
+  ul.appendChild(li);
+  console.log(add, h, m, s, parent);
+};
+
+var prev = document.querySelector(".prev");
+prev.onclick = function() {
+  var chil = ul.children;
+  var count = +document.querySelector("h1").innerHTML;
+  if (count > 0) {
+    chil[count].classList.remove("yellow");
+    chil[count - 1].classList.add("yellow");
+    document.querySelector("h1").innerHTML =
+      +document.querySelector("h1").innerHTML - 1;
+  }
+  console.log(chil, count);
+};
+
+var next = document.querySelector(".next");
+next.onclick = function() {
+  var chil = ul.children;
+  var count = +document.querySelector("h1").innerHTML;
+  if (count < chil.length - 1) {
+    chil[count].classList.remove("yellow");
+    chil[count + 1].classList.add("yellow");
+    document.querySelector("h1").innerHTML =
+      +document.querySelector("h1").innerHTML + 1;
+  }
+  console.log(chil, count);
+};
+
+var del = document.querySelector(".del");
+del.onclick = function() {
+  var chil = ul.children;
+  // var element = document.querySelectorAll("li");
+  var len = Math.ceil(Math.random() * (chil.length - 1));
+  var element = chil[len];
+  ul.removeChild(element);
+};
+
+// add.onclick = timeButton(ul);
+// function timeButton(parent) {
+//   var d = new Date();
+//   var h = d.getHours();
+//   var m = d.getMinutes();
+//   var s = d.getSeconds();
+//   var li = document.createElement("li");
+//   li.innerHTML += `${h}:${m}:${s}`;
+//   parent.appendChild(li);
+//   console.log(add, h, m, s,parent);
+// };
