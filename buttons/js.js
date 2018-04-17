f2f326d2a075df6dba922ba31933fd8cb4054c7e
EvgenStr <juve2910@gmail.com>
2018-04-17 17:51:41 +0300

    game

--- buttons/js.js
+++ buttons/js.js
@@ -2,7 +2,7 @@ var add = document.querySelector(".add");
 var ul = document.querySelector("ul");
 var li = document.createElement("li");
 
-add.onclick = function() {
+add.onclick = function () {
   var d = new Date();
   var h = d.getHours();
   var m = d.getMinutes();
@@ -11,10 +11,14 @@ add.onclick = function() {
   li.innerHTML += `${h}:${m}:${s}`;
   ul.appendChild(li);
   console.log(add, h, m, s, parent);
+  li.addEventListener("click", function () {
+    // this.classList.add("hide");
+   ul.removeChild(this)})
+
 };
 
 var prev = document.querySelector(".prev");
-prev.onclick = function() {
+prev.onclick = function () {
   var chil = ul.children;
   var count = +document.querySelector("h1").innerHTML;
   if (count > 0) {
@@ -27,7 +31,7 @@ prev.onclick = function() {
 };
 
 var next = document.querySelector(".next");
-next.onclick = function() {
+next.onclick = function () {
   var chil = ul.children;
   var count = +document.querySelector("h1").innerHTML;
   if (count < 0) {
@@ -45,7 +49,7 @@ next.onclick = function() {
 };
 
 var del = document.querySelector(".del");
-del.onclick = function() {
+del.onclick = function () {
   var chil = ul.children;
   // var element = document.querySelectorAll("li");
   var len = Math.ceil(Math.random() * (chil.length - 1));
@@ -53,7 +57,14 @@ del.onclick = function() {
   ul.removeChild(element);
 };
 
-// add.onclick = timeButton(ul);
+
+var hide = document.querySelector("ul");
+hide.addEventListener("click", function () {
+  // this.classList.add("hide");
+ ul.removeChild(this);
+});
+
+// add.onclick = timeButton();
 // function timeButton(parent) {
 //   var d = new Date();
 //   var h = d.getHours();
